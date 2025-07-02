1657c5cde29119480db2ab4557912beadcbb806f
Darkknight-86 <bewick.j@northeastern.edu>
2025-07-01 21:25:02 -0400

    add to index.tsx in lab1 and lab2

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,7 @@ export default function Lab1() {
     return (
         <div id="wd-lab1">
             <h2>Lab 1</h2>
+            <p> This is a simple lab component</p>
         </div>
     );
 }
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,5 +2,6 @@ export default function Lab2() {
     return (
         <div id='wd-lab2'>
             <h2>Lab 2</h2>
+            <p>Let's learn about CSS</p>
         </div>
     );}
